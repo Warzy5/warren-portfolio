139c05775ba61f8f7c5a51a8e3a5e38d6cfa7812
Warren Keumeugni <wkeumeumeugni@edenschool.fr>
2026-01-31 12:49:24 +0100

    added resume

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'https://drive.google.com/file/d/1RzgXfUkl_9RNTQRd6WGC67vjU7ScS7aE/view', //paste the link to your resume here
                   '_blank'
                 )
               }
